4391a237db1ac646e68b8ad533edafb8960a4007
Kenny Cason <kenneth.cason@gmail.com>
2016-05-13 23:01:22 -0700

    update index + enhance crawler

--- kissmetrics/spiders/kissmetrics_spider.py
+++ kissmetrics/spiders/kissmetrics_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import os
 
 class KissmetricsSpider(scrapy.Spider):
     name = "kissmetrics"
@@ -6,8 +7,14 @@ class KissmetricsSpider(scrapy.Spider):
     start_urls = [
         "https://blog.kissmetrics.com/page/1/"
     ]
+    pages_crawled = 0
+    max_pages = 3
 
     def parse(self, response):
+        if self.pages_crawled > self.max_pages:
+            print "finished reading " + str(self.pages_crawled - 1) + " pages, finished paging."
+            return
+
         for post_link in response.css(".entry-title a::attr('href')"):
             print "post link: " + post_link.extract()
             yield scrapy.Request(post_link.extract(), self.parse_post)
@@ -16,9 +23,12 @@ class KissmetricsSpider(scrapy.Spider):
         next_page = next_pages[0].extract()
         print "next page: " + next_page
         if next_page:
+            self.pages_crawled += 1
             yield scrapy.Request(next_page, self.parse)
 
     def parse_post(self, response):
-        filename = response.url.split("/")[-2] + '.html'
+        html_dir = os.path.dirname(os.path.realpath(__file__)) + '/../../html/'
+        filename = html_dir + response.url.split("/")[-2] + '.html'
+        print "writing raw html to file -> " + filename
         with open(filename, 'wb') as f:
             f.write(response.body)
